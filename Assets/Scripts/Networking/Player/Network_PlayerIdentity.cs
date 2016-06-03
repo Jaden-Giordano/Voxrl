abb0ece00abe21987125075c2305f08f846b0891
Jaden-Giordano <thejadenjack@gmail.com>
2016-06-03 02:57:10 -0700

    Network Identities (cont.)

--- Assets/Scripts/Networking/Player/Network_PlayerIdentity.cs
+++ Assets/Scripts/Networking/Player/Network_PlayerIdentity.cs
@@ -1,15 +1,49 @@
 ﻿using UnityEngine;
+using UnityEngine.Networking;
 using System.Collections;
 
-public class Network_PlayerIdentity : MonoBehaviour {
+public class Network_PlayerIdentity : NetworkBehaviour {
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+    [SyncVar]
+    public string playerUniqueIdentity;
+    private NetworkInstanceId playerNetID;
+
+    private Transform pTransform;
+
+    public override void OnStartLocalPlayer() {
+        GetNetIdentity();
+        SetIdentity();
+    }
+
+    void Awake() {
+        pTransform = this.transform;
+    }
+
+    void Update() {
+        if (pTransform.name == "" || pTransform.name == "TestPlayer(Clone)")
+            SetIdentity();
+    }
+
+    [Command]
+    void CmdTransmitIdentity(string id) {
+        playerUniqueIdentity = name;
+    }
+
+    [Client]
+    void GetNetIdentity() {
+        playerNetID = GetComponent<NetworkIdentity>().netId;
+        CmdTransmitIdentity(MakeUniqueIdentity());
+    }
+
+    private string MakeUniqueIdentity() {
+        return "[Player " + playerNetID.ToString()+"]";
+    }
+
+    void SetIdentity() {
+        if (!isLocalPlayer)
+            pTransform.name = playerUniqueIdentity;
+        else
+            pTransform.name = MakeUniqueIdentity();
+    }
+
 }
