7087385329a4ca174b36a9c69c02ff0a241462a5
Jaden-Giordano <thejadenjack@gmail.com>
2016-06-10 13:52:51 -0700

    Started Inventory

---
+++ Assets/Scripts/Items/Inventory.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class Inventory {
+
+    protected List<Item> items;
+
+    protected Weapon leftHand;
+    protected Weapon rightHand;
+
+    protected WearableItem leftArm;
+    protected WearableItem rightArm;
+
+    protected WearableItem neck;
+
+    protected WearableItem chest;
+
+    protected WearableItem shoulders;
+
+    protected WearableItem hands;
+
+    protected WearableItem feet;
+
+    public Inventory() {
+        items = new List<Item>();
+    }
+
+}
